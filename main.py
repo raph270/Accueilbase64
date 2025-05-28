97003b06e69bf88455f7510b35af7b2bc1dd9501
raph270 <rapharisbouesso@gmail.com>
2025-05-28 14:38:09 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from google import genai  # Importe la bibliothèque officielle de Google pour interagir avec les modèles Gemini
+import os  # Permet d'accéder aux variables d’environnement
 
+GEMINI_KEY = os.getenv("GEMINI_API_KEY")  # Récupère la clé API Gemini stockée dans les variables d’environnement
+
+client = genai.Client(api_key=GEMINI_KEY)  # Initialise le client Gemini avec la clé API pour authentification
+
+response = client.models.generate_content( 
+    model="gemini-2.0-flash",  
+    contents="Comment vas tu", 
+)
